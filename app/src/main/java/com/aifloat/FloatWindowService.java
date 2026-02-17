a0cd23dab9136314372c973613e629b612041975
User <user@example.com>
2026-02-17 10:14:49 +0800

    Fix: Add missing Bundle import

--- app/src/main/java/com/aifloat/FloatWindowService.java
+++ app/src/main/java/com/aifloat/FloatWindowService.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
